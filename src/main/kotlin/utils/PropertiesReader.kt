09799c07a50fbb7e7fb30fb339fd6faf0d82bd48
Canaan Gifford <canaangifford@gmail.com>
2018-02-26 23:20:39 -0500

    changing PropertiesReader to handle properties as a resource instead of a file

--- src/main/kotlin/utils/PropertiesReader.kt
+++ src/main/kotlin/utils/PropertiesReader.kt
@@ -3,7 +3,7 @@ package utils
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
-import java.io.File
+import java.io.InputStream
 
 /**
  * Builds a list of App Properties from the settings json file.
@@ -19,11 +19,11 @@ class PropertiesReader {
      * Reads the settings file and returns the AppProperties data object.
      */
     fun parseProperties(): AppProperties {
-        val propertiesFile = File("./src/main/resources/app-settings.json")
+        val propertiesFile = PropertiesReader::class.java.getResourceAsStream("/app-settings.json")
         return convert(propertiesFile)
     }
 
-    private fun convert(jsonFile: File): AppProperties {
+    private fun convert(jsonFile: InputStream): AppProperties {
         val mapper = jacksonObjectMapper()
         mapper.configure(DeserializationFeature.ACCEPT_EMPTY_ARRAY_AS_NULL_OBJECT, true)
 
